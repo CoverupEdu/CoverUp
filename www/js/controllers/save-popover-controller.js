546f712adf6159d35ed7a2e39a64af202fce5b54
Oscar O'Brien <oscar.obrien@me.com>
2016-08-25 01:07:15 +0100

    Save button now opens a popover...
    -> Styling this to fit the whole page is really difficult for some reason

--- www/js/controllers/save-popover-controller.js
+++ www/js/controllers/save-popover-controller.js
@@ -3,5 +3,13 @@ Controls the save popover. */
 
 app.controller('save-popover-controller', ['globalData', '$state', 'customFileIO', '$cordovaFile', '$timeout', '$rootScope', '$scope', '$ionicPopover', 'Photo', 'Labels', 'Sets', function(globalData, $state, customFileIO, $cordovaFile, $timeout, $rootScope, $scope, $ionicPopover, Photo, Labels, Sets) {
 	
+	console.log("save popover is running!");
 	
+	function resize() {
+		
+		$('.save_popover_style').css({
+			width: $(window).width(),
+			height: $(window).height()	
+		});	
+	}
 }])
